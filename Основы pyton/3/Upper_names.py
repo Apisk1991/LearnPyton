378dc62b70ab0b45e496497652b30f085e55df87
Apisk1991 <Jaja.from.hell@gmail.com>
2021-10-13 21:57:14 +0300

    Задание с *

--- Основы pyton/3/Upper_names.py
+++ Основы pyton/3/Upper_names.py
@@ -1,3 +1,6 @@
+import re
+import pprint
+
 # f = "Марина"
 # g = f[0]
 # d = dict.fromkeys([g], f)
@@ -6,18 +9,26 @@
 # for key in dict.keys():
 # for key in dict.values():
 
-def thesaurus(*args):
+#словарь, значения которого словарь
+
+def thesaurus_adv(*args):
     result = {}
     for arg in args:
-        gkey = arg[0]
-        if gkey not in result.keys():
-            result[gkey] = [arg]
+        match = re.search(r"([А-Яа-я])[А-Яа-я]+ ([А-Яа-я])[А-Яа-я]+", arg)
+        surname_key = match.group(2)
+        name_key = match.group(1)
+        if surname_key not in result.keys():
+            result[surname_key] = {name_key: [arg]}
         else:
-            result[gkey].append(arg)
+            if name_key not in result[surname_key].keys():
+                result[surname_key][name_key] = [arg]
+            else:
+                result[surname_key][name_key].append(arg)
     return result
 
 
-print(thesaurus("Марина", "Иван", "Гога", "Миша"))
+pprint.pprint(thesaurus_adv("Иван Сергеев", "Алла Сидорова", "Инна Серова", "Петр Алексеев",
+                            "Илья Иванов", "Анна Савельева", "Василий Суриков"))
 
 
 
